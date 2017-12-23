1d5c88a29dad649b8bd6831833ba3ecf398edcf6
game1995 <game_130@hotmail.com>
2017-12-23 19:29:03 +0700

    Testing IS Data

---
+++ src/ooptest/HelloWorld.java
@@ -0,0 +1,12 @@
+package ooptest;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world,I'm Game");
+        //  Add Data
+        System.out.println("Chaimongkol");
+        System.out.println("Information System");
+        System.out.println("Faculty of Management Technology,RMUTSV");
+
+    }//main
+}//class
